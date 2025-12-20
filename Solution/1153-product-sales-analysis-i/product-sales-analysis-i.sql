0ad21cdf7f90dccdfe7f2855ea05b4b5376dbf93
Raunak Yadav <155085732+Raunak7888@users.noreply.github.com>
2025-12-20 16:21:57 +0530

    Time: 1198 ms (55.19%) | Memory: 0B (100.00%) - LeetSync

--- Solution/1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ Solution/1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,4 +1,7 @@
-select t1.product_name,t2.year,t2.price
-from Sales as t2 
-INNER join Product as t1
-on t1.product_id = t2.product_id;
+-- select t1.product_name,t2.year,t2.price
+-- from Sales as t2 
+-- INNER join Product as t1
+-- on t1.product_id = t2.product_id;
+SELECT product_name, year, price FROM Sales
+JOIN Product
+USING(product_id);
