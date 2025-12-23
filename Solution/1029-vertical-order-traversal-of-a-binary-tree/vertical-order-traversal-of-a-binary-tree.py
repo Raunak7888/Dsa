84d499e48e3e0e8398a3e4d01765e3897b3d991d
Raunak Yadav <155085732+Raunak7888@users.noreply.github.com>
2025-12-24 00:31:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (100.00%) - LeetSync

--- Solution/1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ Solution/1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -1,26 +1,29 @@
-from collections import defaultdict
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def verticalTraversal(self, root):
-        nodes = []
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        self.in_order = []
+        self.traverse(root, 0, 0)
+        self.in_order.sort()
 
-        def dfs(node, row, col):
-            if not node:
-                return
-            nodes.append((col, row, node.val))
-            dfs(node.left, row + 1, col - 1)
-            dfs(node.right, row + 1, col + 1)
-
-        dfs(root, 0, 0)
-        nodes.sort()
-
+        prev_col = float("-inf")
         res = []
-        prev_col = float('-inf')
-
-        for col, row, val in nodes:
+        for col, row, val in self.in_order:
             if col != prev_col:
                 res.append([])
                 prev_col = col
             res[-1].append(val)
-
+        
         return res
+
+    def traverse(self, node: TreeNode, row: int, col: int):
+        if not node:
+            return
+        
+        self.traverse(node.left, row+1, col-1)
+        self.in_order.append((col, row, node.val))
+        self.traverse(node.right, row+1, col+1)
