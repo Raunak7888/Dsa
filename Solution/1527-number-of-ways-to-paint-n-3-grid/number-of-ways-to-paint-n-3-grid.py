dc97975413b35d3ae559e3f33eb2cdd48240638e
Raunak Yadav <155085732+Raunak7888@users.noreply.github.com>
2026-01-03 19:19:32 +0530

    Time: 7 ms (83.84%) | Memory: 17.3 MB (96.04%) - LeetSync

--- Solution/1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.py
+++ Solution/1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.py
@@ -2,10 +2,10 @@ class Solution:
     def numOfWays(self,n: int) -> int:
         MOD = 10**9 + 7
 
-        A = 6  # Type A rows at row 1
-        B = 6  # Type B rows at row 1
+        A = 6  
+        B = 6  
 
-        for _ in range(2, n + 1):
+        for _ in range(1, n):
             newA = (3 * A + 2 * B) % MOD
             newB = (2 * A + 2 * B) % MOD
             A, B = newA, newB
