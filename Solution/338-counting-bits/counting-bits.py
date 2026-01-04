3f4bee5acd09229435fb24eff14fa604017fd986
Raunak Yadav <155085732+Raunak7888@users.noreply.github.com>
2026-01-04 22:11:05 +0530

    Time: 3 ms (94.90%) | Memory: 18.3 MB (79.30%) - LeetSync

--- Solution/338-counting-bits/counting-bits.py
+++ Solution/338-counting-bits/counting-bits.py
@@ -2,8 +2,5 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         res = [0]*(n+1)
         for i in range(n+1):
-            b = bin(i)
-            for k in str(b):
-                if k == "1":
-                    res[i] += 1
+            res[i] = res[i//2]+(i%2)
         return res
