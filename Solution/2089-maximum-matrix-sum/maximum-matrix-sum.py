26d18e6c1cfb1a435d1adc335eca846a6b5b1f48
Raunak Yadav <155085732+Raunak7888@users.noreply.github.com>
2026-01-05 23:14:18 +0530

    Time: 112 ms (17.72%) | Memory: 26.6 MB (15.19%) - LeetSync

--- Solution/2089-maximum-matrix-sum/maximum-matrix-sum.py
+++ Solution/2089-maximum-matrix-sum/maximum-matrix-sum.py
@@ -3,15 +3,15 @@ class Solution:
         n = len(m)
         res = 0
         minVal = 9999999
-        r = []
+        r = 0
         for i in range(n):
             for j in range(n):
                 res += abs(m[i][j])
                 minVal = min(minVal, abs(m[i][j]))
                 if m[i][j]<0:
-                    r.append(m[i][j])
+                    r+=1
         print(res,minVal,r)
-        if len(r)%2==0:
+        if r%2==0:
             return res
         else: 
             return res-minVal*2
