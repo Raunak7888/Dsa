00c4a4f8ecdf10a0adc0a0b64392cd5d2a44c7d0
Raunak Yadav <155085732+Raunak7888@users.noreply.github.com>
2026-01-07 20:55:02 +0530

    Time: 46 ms (84.41%) | Memory: 45.4 MB (5.32%) - LeetSync

--- Solution/1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.py
+++ Solution/1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.py
@@ -4,15 +4,38 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+def get_subtree_sum(node: TreeNode, subtree_sums: list):
+    if node is None:
+        return 0
+
+    total_sum = node.val
+
+    total_sum += get_subtree_sum(node.left, subtree_sums)
+
+    total_sum += get_subtree_sum(node.right, subtree_sums)
+
+    subtree_sums.append(total_sum)
+
+    return total_sum
+
+
+def get_max_product(node: TreeNode):
+    subtree_sums = list()
+
+    total_sum = get_subtree_sum(node, subtree_sums)
+
+    max_product = 0
+
+    for s in subtree_sums:
+        product = s * (total_sum - s)
+
+        if product > max_product:
+            max_product = product
+
+    return max_product % (10**9 + 7)
+
+
 class Solution:
     def maxProduct(self, root: Optional[TreeNode]) -> int:
-        ans, total=-inf, 0
-        def dfs(root):
-            nonlocal ans, total
-            if not root: return 0
-            Sum=root.val+dfs(root.left)+dfs(root.right)
-            ans=max(ans, (total-Sum)*Sum)
-            return Sum
-        total=dfs(root)
-        dfs(root)
-        return ans%(10**9+7)
+        return get_max_product(root)
