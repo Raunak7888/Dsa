ce80f200edab06828c1ba25ec1e5dfc2c3f1388c
Raunak Yadav <155085732+Raunak7888@users.noreply.github.com>
2026-01-08 12:44:17 +0530

    Time: 44 ms (97.09%) | Memory: 32.4 MB (92.72%) - LeetSync

---
+++ Solution/2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+        n, j, s = len(nums), 0, 0
+        while j < n:
+            odd = even = 0
+            while j + 1 < n and nums[j] > nums[j + 1]:
+                if j & 1: odd += nums[j]
+                else: even += nums[j]
+                j += 1
+            s += (odd if j & 1 else even) + nums[j]
+            j += 2
+        return s
